0949393f447776a8d2fa7043fff5bcf3fc043ee3
nagendra539 <nagendra.chunduru@gmail.com>
2026-01-29 14:25:23 +0530

    terraform practice, added ec2 and sg

---
+++ ec2/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+    name = "allow_ssh_terraform"
+    description = "Security group to allow SSH access from anywhere"
+    
+    egress  {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"] 
+    }
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # allow SSH from anywhere
+        ipv6_cidr_blocks = ["::/0"] # allow SSH from anywhere (IPv6)
+    }
+    tags = {
+        Name = "terraform-managed"
+    }
+}
+
+resource "aws_instance" "terraform" {
+    ami           = "ami-0220d79f3f480ecf5" # devops practice
+    instance_type = "t3.small"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = "terraform-instance" 
+    }
+  
+}
